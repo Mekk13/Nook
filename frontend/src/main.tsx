d02655850d42678dbff7f0fa0c99362183118c0a
Mekk13 <mariasuciu313@gmail.com>
2026-06-04 11:56:26 +0300

    responsive css pass across all pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import './responsive.css'
 import App from './App.tsx'
 import { startNetworkMonitor } from './stores/useNetworkStore.ts';
 import '@tabler/icons-webfont/dist/tabler-icons.min.css';
